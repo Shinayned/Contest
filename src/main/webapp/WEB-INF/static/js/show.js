01cddb885bf86e0643bbf38355d6feae12958b4a
mell-old <oleh.melnechyk@gmail.com>
2019-02-11 18:55:08 +0200

    Testing please show.js)

---
+++ src/main/webapp/WEB-INF/static/js/show.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	var View = $('.showFilesList').clone(true);
+	$("#start").remove();
+
+	View.attr("id", "2sadasfas12e3");
+	View.find('#text').text('txt.pdf');
+	$('#FilesList').append(View);
+
+
+
+//--------Це затестити треба----------
+	var FileListsArray = [];
+	FileListsArray = $.POST("/drive/upload");
+	console.log(FileList);
+
+	//$('#FilesList').append(View);
+	//document.querySelector("body").appendChild(ViewFiles);
+});
