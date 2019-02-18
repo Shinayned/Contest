cd01a9ba0e5567a3ea334ce8db4368818adc5348
Mell <oleh.melnechyk@gmail.com>
2019-02-18 23:35:39 +0200

    Final fixed download

--- src/main/webapp/WEB-INF/static/js/event.js
+++ src/main/webapp/WEB-INF/static/js/event.js
@@ -1,9 +1,11 @@
 $(".save").click(function(e) {
-	//var id = $(this).closest("div[title]").attr("id");
+	var id = $(this).closest("div[title]").attr("id");
 	var name = $(this).closest("div[title]").attr("name");
-	
-	this.href = $.get("drive/download?id=" + id);
-	//return false;
+	console.log(name);
+	//$.get("drive/download?id=" + id);
+	this.href = name;
+	console.log(this.href);
+	return false;
     });
 
 $(".remove").click(function(e){
