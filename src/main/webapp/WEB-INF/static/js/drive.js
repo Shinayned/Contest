994faf4847a979fefac288be5e2ce5575617ceb9
Mell <oleh.melnechyk@gmail.com>
2019-03-07 17:33:32 +0200

    fix

--- src/main/webapp/WEB-INF/static/js/drive.js
+++ src/main/webapp/WEB-INF/static/js/drive.js
@@ -1,10 +1,4 @@
 $(document).ready(function () {
-    $body = $("body");
-
-$(document).on({
-    ajaxStart: function() { $body.addClass("loading");    },
-     ajaxStop: function() { $body.removeClass("loading"); }    
-});
     var dropZone = $('#upload-container');
 
     $('#file-input').focus(function () {
