3baf9207c6c83a264025e56deaad8514d86d221b
Maksym <rim.maxim@gmail.com>
2019-03-07 13:58:34 +0200

    Application prerelease developing. - fixes and improves;

--- src/main/java/converter/DateTimeConverter.java
+++ src/main/java/converter/DateTimeConverter.java
@@ -1,6 +1,8 @@
 package converter;
 
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +10,14 @@ import org.springframework.stereotype.Component;
 public class DateTimeConverter implements Converter<String, DateTime> {
     @Override
     public DateTime convert(String source) {
-        if (source == null || source.isEmpty()){
+        if (source == null || source.isEmpty())
             return null;
-        }
+
+        String datePlusTime = "yyyy-MM-dd HH:mm";
+        DateTimeFormatter formatter = DateTimeFormat.forPattern(datePlusTime);
+
+        if (source.length() == datePlusTime.length())
+            return DateTime.parse(source, formatter);
 
         return new DateTime(source);
     }
