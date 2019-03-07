e8a6173a639ec0044adeedeb425217775ea2ab60
Mell <oleh.melnechyk@gmail.com>
2019-03-07 11:20:04 +0200

    fixed forms

--- src/main/webapp/WEB-INF/static/js/adminControler.js
+++ src/main/webapp/WEB-INF/static/js/adminControler.js
@@ -76,3 +76,21 @@ $(document).ready(function () {
         return false;
     });
 });
+
+$("#data-end").click(function(e){
+    var datatime = $("#expirationTime").val().split("-");
+    var endDate = new Date(datatime[0],datatime[1],datatime[0]);
+    var todayDate = new Date();
+    if(endDate != "" && endDate > todayDate)
+    {
+        $.ajax({
+        type: "POST",
+        url: "",
+        data: "contestId="+contestId&"expirationTime="+datatime,
+            success: function () {
+                    alert("Термін дії зміненно");
+                }        
+    });        
+    }
+    return false;
+})
