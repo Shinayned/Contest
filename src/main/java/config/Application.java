2b788a7d867324445b2330ba3655df72ceac3eda
Maksym <rim.maxim@gmail.com>
2019-04-15 19:18:00 +0300

    Application prerelease developing. - minor fixes and improves;

--- src/main/java/config/Application.java
+++ src/main/java/config/Application.java
@@ -76,6 +76,74 @@ public class Application {
         contestService.createContest(contest);
     }
 
+    public Contest firstContest() {
+        String contestName = "Конкурс на здобуття стипендій Кабінету Міністрів України для молодих вчених.";
+        String contestTitle = contestName;
+        String contestDescription = "Дата проведення: двічі на рік - до 1 квітня та до 1 жовтня, про що не пізніше, ніж за три місяці повідомляються за підпорядкуванням наукові установи, організації і вищі навчальні заклади.\n" +
+                "Хто може прийняти участь : наукові працівники, науково-педагогічні працівники, аспіранти, ад’юнкти, докторанти, інші вчені та інженерно-технічні працівники наукових установ, організацій, вищих навчальних закладів. \n" +
+                "Вік учасників: не може перевищувати 33 років для осіб, які мають вищу освіту не нижче другого (магістерського) рівня, та 38 років для осіб, які мають науковий ступінь доктора наук або навчаються в докторантурі, на час їх висування для участі у конкурсі на здобуття стипендій.\n";
+        String imgUrl = "/static/img/stipendium.png";
+        String pageUrl = "http://www.kdpu-nt.gov.ua/content/umovi-ta-poryadok-provedennya-konkursu-na-zdobuttya-stipendiy-kabinetu-ministriv-ukrayini-dl?fbclid=IwAR3VFbvxZUQN5MINi8sJodD9NEp3nkm2FUeAEnqRapH1f5i8DUCbVXM0jWk";
+
+        Form files = new FileForm("Завантажити файл");
+
+        List<Form> forms = new ArrayList<>();
+        forms.add(files);
+
+        Contest contest = new Contest(contestName, forms, contestTitle, contestDescription, imgUrl, pageUrl);
+        return contest;
+    }
+
+    public Contest secondContest() {
+        String contestName = "Стипендії Президента України для молодих вчених";
+        String contestTitle = contestName;
+        String contestDescription = "Дата проведення: двічі на рік (до 25 квітня і 25 жовтня)\n" +
+                "Хто може прийняти участь: аспіранти, наукові працівники науково-дослідних інститутів та інших наукових установ, організацій, підприємств (обсерваторій, ботанічних садів, дендропарків, заповідників, бібліотек, музеїв) (далі - організації), що перебувають у віданні Національної академії наук.\n" +
+                "Вік учасників: не більше 35 років для осіб, які мають вищу освіту не нижче другого (магістерського) рівня, та 40 років для осіб, які мають науковий ступінь доктора наук або навчаються в докторантурі, на час їх висування для участі у конкурсі на здобуття стипендій Президента України.\n";
+        String imgUrl = "/static/img/stipendium.png";
+        String pageUrl = "http://kdpu-nt.gov.ua/content/polozhennya-pro-stipendiyi-prezidenta-ukrayini-dlya-molodikh-vchenikh";
+
+        Form files = new FileForm("Завантажити файл");
+
+        List<Form> forms = new ArrayList<>();
+        forms.add(files);
+
+        Contest contest = new Contest(contestName, forms, contestTitle, contestDescription, imgUrl, pageUrl);
+        return contest;
+    }
+
+    public Contest thirdContest() {
+        String contestName = "Премія Верховної Ради України";
+        String contestTitle = contestName;
+        String contestDescription = "Дата проведення: до 15 березня\n" +
+                "Хто може прийняти участь: \n" +
+                "Вік учасників: не менше 35 років\n";
+        String imgUrl = "/static/img/stipendium.png";
+        String pageUrl = "http://www.kdpu-nt.gov.ua/content/umovi-ta-poryadok-provedennya-konkursu-na-zdobuttya-stipendiy-kabinetu-ministriv-ukrayini-dl?fbclid=IwAR3VFbvxZUQN5MINi8sJodD9NEp3nkm2FUeAEnqRapH1f5i8DUCbVXM0jWk";
+
+        Form education = new StringForm("Освіта", "Освіта");
+        Form specialty = new StringForm("Спеціальність", "Спеціальність");
+        Form degree = new StringForm("Почесні звання", "Почесні звання");
+        Form academicDegree = new StringForm("Науковий ступінь", "Науковий ступінь");
+        Form scientistStatus = new StringForm("Вчене звання", "Вчене звання");
+        Form amountOfScienceWorks = new StringForm("Кількість наукових праць", "Кількість наукових праць");
+        Form identificationCode = new StringForm("Індивідуальний ідентифікаційний номер", "Індивідуальний ідентифікаційний номер");
+        Form files = new FileForm("Завантажити файл");
+
+        List<Form> forms = new ArrayList<>();
+        forms.add(education);
+        forms.add(specialty);
+        forms.add(degree);
+        forms.add(academicDegree);
+        forms.add(scientistStatus);
+        forms.add(amountOfScienceWorks);
+        forms.add(identificationCode);
+        forms.add(files);
+
+        Contest contest = new Contest(contestName, forms, contestTitle, contestDescription, imgUrl, pageUrl);
+        return contest;
+    }
+
     public static List<Form> testForms() {
         List<Form> forms = new ArrayList<>();
 
